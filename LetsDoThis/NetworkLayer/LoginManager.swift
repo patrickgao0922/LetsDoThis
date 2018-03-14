df7fc818285bf42968e6126a7f66a299ca5027c0
Patrick Gao <patrickgao0922@gmail.com>
2018-03-14 14:44:44 +1100

    finish login test

--- LetsDoThis/NetworkLayer/LoginManager.swift
+++ LetsDoThis/NetworkLayer/LoginManager.swift
@@ -8,7 +8,21 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
-class loginManager {
-//    static func login(with username:)
+class LoginManager {
+    static let sharedInstance = LoginManager()
+    
+    /// Login and obtain accessToken and refreshToken using username and password
+    ///
+    /// - Parameters:
+    ///   - username: Email
+    ///   - password: Password
+    /// - Returns: Single Observable with results of token dictionary
+    func login(with username:String, password: String) -> Single<[String:String]>{
+        return Single<[String:String]>.create(subscribe: { (single) -> Disposable in
+            
+            return Disposables.create()
+        })
+    }
 }
--- LetsDoThisTests/Login.swift
+++ LetsDoThisTests/Login.swift
@@ -9,14 +9,36 @@
 import Nimble
 import Quick
 @testable import LetsDoThis
+import RxSwift
 
 class OAuthTests:QuickSpec {
     override func spec() {
+        var loginManager:LoginManager!
+        var disposeBag = DisposeBag()
         describe("OAuth Login Networking Layer") {
+            func setup() {
+                loginManager = LoginManager.sharedInstance
+            }
+            beforeEach {
+                setup()
+            }
             it("Login in with username(email) and password") {
                 let username = "test@test.com"
                 let password = "test"
+                let loginSingle = loginManager.login(with: username, password: password)
                 
+                loginSingle.subscribe({ (single) in
+                    switch single {
+                    case .success(let result):
+                        var accessToken = result["access_token"]
+                        var refreshToken = result["refresh_token"]
+                        expect(accessToken).toNot(beNil())
+                        expect(refreshToken).toNot(beNil())
+                    default:
+                        break
+                    }
+                }).disposed(by: disposeBag)
+                self.waitForExpectations(timeout: 4.0, handler: nil)
             }
         }
     }
