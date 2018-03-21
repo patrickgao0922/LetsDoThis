8900f90225cc293392f91987d880089d413a4781
Patrick Gao <patrickgao0922@gmail.com>
2018-03-22 09:53:27 +1100

    commit

---
+++ LetsDoThis/UIComponents/ActivityTypeCVC.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityTypeCVC.swift
+//  LetsDoThis
+//
+//  Created by Patrick Gao on 22/3/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTypeCVC: UICollectionViewCell {
+    
+}
