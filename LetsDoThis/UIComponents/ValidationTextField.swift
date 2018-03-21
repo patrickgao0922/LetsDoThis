ccdf9e1513372bb6d6e21f085ce7e38e6b1807c4
Patrick Gao <patrickgao0922@gmail.com>
2018-03-21 15:50:58 +1100

    commit

---
+++ LetsDoThis/UIComponents/ValidationTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ValidationTextField.swift
+//  LetsDoThis
+//
+//  Created by Patrick Gao on 21/3/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import UIKit
+
+class ValidationTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
