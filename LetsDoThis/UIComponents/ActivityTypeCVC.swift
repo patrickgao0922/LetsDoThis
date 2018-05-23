9810f504662918c38b396ab0239647ea47858aaa
Patrick Gao <patrickgao0922@gmail.com>
2018-05-24 09:09:09 +1000

    commit

---
+++ LetsDoThis/UIComponents/ActivityTypeCVC.swift
@@ -0,0 +1,19 @@
+//
+//  ActivityTypeCVC.swift
+//  LetsDoThis
+//
+//  Created by Patrick Gao on 22/3/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ActivityTypeCVC: UICollectionViewCell {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var activityTypeIconImageView: UIImageView!
+    @IBOutlet var activityNameLabel: UILabel!
+    
+    
+}
